1b206929adc1853e337e374e321e54bd55e91ebd
shafay07 <32669422+shafay07@users.noreply.github.com>
2019-03-30 21:06:38 +0800

    plotly graph
    added ist graph

--- main.js
+++ main.js
@@ -4,6 +4,33 @@ $( document ).ready(function() {
 });
 
 function draw_graph(){
+    var data_set;
+    var date = [];
+    var population = [];
+    //loading csv file
+    d3.csv("population.csv").then(function(data) {
+        data_set = data;
+        //preprocess the data
+        for(i=0;i<data_set.length;i++)
+        {
+            date.push(data_set[i].Date);
+            population.push(data_set[i].MalaysiaPopulation);
+        }
+        //draw the graph
+        var data = [
+            {
+              x: date,
+              y: population,
+              type: 'bar'
+            }
+        ];
+        Plotly.newPlot('graph', data);
+    });
+    
+
+
+    
+    
    
 
 }
